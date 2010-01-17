c02ceb29cfb0ba5186d3da1640f40df66cbddbec
Philip (flip) Kromer <flip@infochimps.org>
2010-01-16 18:00:36 -0600

    initial commit from cartilage

---
+++ init.rb
@@ -0,0 +1,36 @@
+::ROOT_DIR = File.expand_path(File.dirname(__FILE__)) unless defined? ::ROOT_DIR
+# require "rubygems"
+
+begin
+  require "vendor/dependencies/lib/dependencies"
+rescue LoadError
+  require "dependencies"
+end
+require "monk/glue"
+require "json"
+require 'sinatra'
+require 'haml'
+# require 'dm-core'
+# require 'dm-validations'
+# require 'dm-timestamps'
+
+class Main < Monk::Glue
+  set :app_file, __FILE__
+  use Rack::Session::Cookie,
+    :key          => 'rack.session',
+    :domain       => settings(:domain),
+    :path         => '/',
+    :expire_after => 2592000,
+    :secret       => settings(:session_secret)
+end
+
+# Load all application files.
+Dir[Monk::Glue.root_path("app/**/*.rb")].each do |file|
+  require file
+end
+
+# # Connect to database.
+# sqlite3_path = settings(:sqlite3)[:database]
+# DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/#{sqlite3_path}")
+
+Main.run! if Main.run?
---
+++ unicorn-conf.rb
@@ -0,0 +1,16 @@
+ROOT_DIR = '/var/www/cartilage/shared'
+FileUtils.mkdir_p  ROOT_DIR+'/tmp'
+FileUtils.mkdir_p  ROOT_DIR+'/log'
+
+# 16 workers and 1 master
+worker_processes  5
+preload_app       true
+timeout           80
+# listen          8080, :tcp_nopush => true
+listen            ROOT_DIR+'/tmp/unicorn.sock', :backlog => 2048
+pid               ROOT_DIR+'/tmp/unicorn.pid'
+stderr_path       ROOT_DIR+'/log/unicorn.stderr.log'
+stderr_path       ROOT_DIR+'/log/unicorn.stdout.log'
+
+# # REE
+GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=)
