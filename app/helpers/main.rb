01114482d3a238a3d4798a4fe7e55270b94f37a8
Philip (flip) Kromer <flip@infochimps.org>
2010-01-16 21:05:18 -0600

    Images added

--- app/helpers/main.rb
+++ app/helpers/main.rb
@@ -1,11 +1,11 @@
 class Main < Monk::Glue
   helpers do
-    RANDOM_IMG_PATH='images/r'
+    RANDOM_IMG_PATH='images/180px'
     PUBLIC_ROOT_PATH=File.dirname(__FILE__)+"/../../public"
 
     def random_img handle
-      filename = File.basename( Dir[File.join(PUBLIC_ROOT_PATH, RANDOM_IMG_PATH, '*')].to_a.random )
-      image_tag( '/'+File.join(RANDOM_IMG_PATH, filename), handle )
+      filename = File.basename( Dir[File.join(PUBLIC_ROOT_PATH, RANDOM_IMG_PATH, handle.to_s, '*')].to_a.random )
+      image_tag( '/'+File.join(RANDOM_IMG_PATH, handle.to_s, filename), handle )
     end
   end
 end
