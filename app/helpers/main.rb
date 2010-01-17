ddc02636edef8c241a317e251450307e23382a25
Philip (flip) Kromer <flip@infochimps.org>
2010-01-16 23:12:05 -0600

    made app customizable from settings.yml

--- app/helpers/main.rb
+++ app/helpers/main.rb
@@ -1,11 +1,10 @@
 class Main < Monk::Glue
   helpers do
-    RANDOM_IMG_PATH='images/180px'
     PUBLIC_ROOT_PATH=File.dirname(__FILE__)+"/../../public"
 
     def random_img handle
-      filename = File.basename( Dir[File.join(PUBLIC_ROOT_PATH, RANDOM_IMG_PATH, handle.to_s, '*')].to_a.random )
-      image_tag( '/'+File.join(RANDOM_IMG_PATH, handle.to_s, filename), handle )
+      filename = File.basename( Dir[File.join(PUBLIC_ROOT_PATH, settings(:random_img_path), handle.to_s, '*')].to_a.random )
+      image_tag( '/'+File.join(settings(:random_img_path), handle.to_s, filename), handle )
     end
   end
 end
